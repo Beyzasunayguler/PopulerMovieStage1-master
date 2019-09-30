b2e99189cbce930c805a6be47121d41a167af13c
beyzasunay <beyzasguler@gmail.com>
2019-09-30 11:11:46 +0300

    add favouriteadapter

--- app/src/main/java/com/example/populermoviestage1/Database/AppDatabase.java
+++ app/src/main/java/com/example/populermoviestage1/Database/AppDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
-import com.example.populermoviestage1.MainActivity;
+
 
 @Database(entities = {Movie.class}, version = 1, exportSchema = false)
 
